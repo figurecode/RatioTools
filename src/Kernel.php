e8187acb7f5acae1a283f680512ca79b7af96cab
Dmitriy Matyuha <figurecode@gmail.com>
2018-12-06 17:30:50 +0300

    Добавлено описание пакета. Добавлен каталог src. Добавлена болванка класса Kernel

---
+++ src/Kernel.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by figurecode.ru
+ * User: Matyuha Dmitriy <figurecode@gmail.com>
+ * Date: 06.12.2018
+ */
+
+namespace Ratio\Tools;
+
+/**
+ * Class Kernel
+ * @package Ratio\Tools
+ * @author  Matyuha Dmitriy <figurecode@gmail.com>
+ */
+class Kernel
+{
+
+}
